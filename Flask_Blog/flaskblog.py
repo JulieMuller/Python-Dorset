e89f395ab413fff206a07eb3b646660a98355f04
Julie <julie@example.com>
2023-11-24 17:53:29 +0000

    html

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request, flash
+import sys
 app = Flask(__name__)
 
 users = {'user1': 'password1', 'user2': 'password2'}
@@ -12,7 +13,7 @@ def hello():
 #whatever written under will be saved in the directory above
 
 def home():
-    return render_template('login3.html')
+    return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -21,10 +22,14 @@ def login():
 
     if username in users and users[username] == password:
         # Successful login
+        print("username : %s | mdp : %s" %(username, password), file=sys.stderr)
         return render_template('welcomepage.html')
     else:
         # Failed login
+        print("username : %s | mdp : %s" %(username, password), file=sys.stderr)
+        app.logger.info('testing info log')
         return 'Invalid username or password. Please try again.'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
